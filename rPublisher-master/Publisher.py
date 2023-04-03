c52364a1dc62e9d66f23317cedad51173c9546a1
Jonathan Sekela <jonathan.sekela@gmail.com>
2023-04-03 12:51:09 -0400

    first commit

---
+++ rPublisher-master/Publisher.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+import pika
+
+
+def pub(virtualhost, message, exchange_name):
+
+    # Set the connection parameters to connect to rabbit-server1 on port 5672
+    # on the / virtual host using the username "guest" and password "guest"
+    username = 'guest'
+    password = 'guest'
+    hostname = ''
+
+    credentials = pika.PlainCredentials(username, password)
+    parameters = pika.ConnectionParameters(hostname,
+                                           5672,
+                                           virtualhost,
+                                           credentials)
+
+    connection = pika.BlockingConnection(parameters)
+
+    channel = connection.channel()
+
+    exchange_name = 'patient_data'
+
+    channel.exchange_declare(exchange=exchange_name, exchange_type='topic')
+
+    routing_key = 'patient.info'
+
+    channel.basic_publish(
+        exchange=exchange_name, routing_key=routing_key, body=message)
+    print(" [x] Sent %r:%r" % (routing_key, message))
+
+    connection.close()
+
+    def loadApps():
+        for dir in _execommon.APPSDIR.iterdir():
+            if not dir.is_dir():
+                continue
+            dirName = dir.name
+            # logging.debug(f'Loading app "{dirName}".')
+            moduleName = f'apps.{dirName}.app'
+            try:
+                module = importlib.import_module(moduleName)
+            except ImportError:
+                logging.exception(
+                    f'Error while trying to import app "{moduleName}":')
+                continue
+            if dirName == 'sys':
+                flaskconst.flaskapp.APP.register_blueprint(module.c.BLUEPRINT)
+            else:
+                flaskconst.flaskapp.APP.register_blueprint(
+                    module.c.BLUEPRINT,
+                    url_prefix=f'/{dirName}',
+                )
+            flaskconst.APPS[dirName] = module
+        # logging.debug('Synchronizing privilege entities...')
+        appSys = flaskconst.APPS['sys']
+        sysDbVer = appSys.c.DBVLATEST.V.versionStats()[0]
+
+        # Only do entity sync if the database is ready for it.
+        if sysDbVer is not None and sysDbVer >= 13:
+            entities = {entityType: set()
+                        for entityType in appSys.c.ENTITYTYPE}
+            for app in flaskconst.APPS.values():
+                for entityType in appSys.c.ENTITYTYPE:
+                    entities[entityType].update(
+                        app.c.ENTITIES.get(entityType, {}))
+            appSys.db.accessEntitiesSet(entities)
+
+        # Only do admin account creation if the database is ready for it.
+        if sysDbVer is not None and sysDbVer >= 14:
+            try:
+                appSys.db.accountGetByUsername(appSys.c.ACCOUNTUSERNAMESTARTER)
+            except KeyError:
+                appSys.db.accountNew(
+                    appSys.c.ACCOUNTUSERNAMESTARTER,
+                    'Delete',
+                    'Me',
+                    'Delete This Account',
+                    security.LOGINMETHODS.BASIC,
+                    # Change this arg to a strong `str` password.  Intentionally `None` to throw exception.
+                    None,
+                    _BYPASSAUTHORIZATION=True,
+                )
+        logging.info(
+            f'App loading complete.  Current list is: {flaskconst.APPS!r}')
---
+++ rSubscriber-master/Subscriber.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import pika
+import sys
+import json
+
+# Set the connection parameters to connect to rabbit-server1 on port 5672
+# on the / virtual host using the username "guest" and password "guest"
+
+username = 'guest'
+password = 'guest'
+hostname = ''
+virtualhost = ''
+
+
+credentials = pika.PlainCredentials(username, password)
+parameters = pika.ConnectionParameters(hostname,
+                                       5672,
+                                       virtualhost,
+                                       credentials)
+
+connection = pika.BlockingConnection(parameters)
+
+channel = connection.channel()
+
+exchange_name = 'patient_data'
+channel.exchange_declare(exchange=exchange_name, exchange_type='topic')
+
+result = channel.queue_declare('', exclusive=True)
+queue_name = result.method.queue
+
+binding_keys = "#"
+
+if not binding_keys:
+    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
+    sys.exit(1)
+
+for binding_key in binding_keys:
+    channel.queue_bind(
+        exchange=exchange_name, queue=queue_name, routing_key=binding_key)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+
+    testing_data = json.loads(body)
+    for test in testing_data:
+        print("*Python Class*")
+        print("\ttesting_id: " + str(test['testing_id']))
+        print("\tpatient_name: " + str(test['patient_name']))
+        print("\tpatient_mrn: " + str(test['patient_mrn']))
+        print("\tpatient_zipcode: " + str(test['patient_zipcode']))
+        print("\tpatient_status: " + str(test['patient_status']))
+        print("\tcontact_list: " + str(test['contact_list']))
+        print("\tevent_list: " + str(test['event_list']))
+
+
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
